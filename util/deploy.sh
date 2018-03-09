9014b438ec0183c8b700519d561aedc341ebce12
Adam M. Jacobs <adam.jacobs@stonybrook.edu>
2018-03-08 22:07:44 -0500

    tweak deploy script

--- util/deploy.sh
+++ util/deploy.sh
@@ -3,4 +3,8 @@
 host_domain=amjacobs.net
 sync_command="cd web && date | tee -a gitsync.out gitsync.err > /dev/null && git pull 1>>gitsync.out 2>>gitsync.err"
 
+jekyll build
+git commit -am'Rebuild site.'
+git push
+
 ssh $host_domain "$sync_command"
